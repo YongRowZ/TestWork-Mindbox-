b7d34f136c548c89ff3835adf43592793a86e8f1
YongRowZ <yongrowz@gmail.com>
2023-11-27 12:07:34 +0500

    Добавлен интерфейс IFigure

--- TestLIB(Mindbox)/Figures/Circle.cs
+++ TestLIB(Mindbox)/Figures/Circle.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestLIB_Mindbox_.Interfaces;
 
 namespace TestLIB_Mindbox_.Figures
 {
-    public class Circle : Figure
+    public class Circle : IFigure<double>
     {
-        public double Radius { get; private set; }
-        
-        /// <param name="radius">Радиус круга</param>
-        public Circle(double radius)
+        public double Area { get; set; }
+        public double Radius { get; set; }
+
+        public Circle(double radius) 
         {
-            SetArea(MatchArea(radius));
+            Radius = 
+                radius < 0 ? Radius : radius;
+
+            Area = 
+                !IsValid() ? Area : MathArea();
+        }
+
+        public bool IsValid()
+        {
+            return Radius > 0;
+        }
+
+        public double MathArea()
+        {
+            double area = default;
+            return Radius <= 0 ?
+                area : Math.PI * Radius * Radius;
         }
     }
 }
---
+++ TestLIB(Mindbox)/Interfaces/IFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLIB_Mindbox_.Interfaces
+{
+    public interface IFigure<T>
+    {
+        T Area { get; set; }
+        T MathArea();
+        bool IsValid();
+    }
+}
